c36831142aa6ebf378090a53b7d3412d4779d7ff
Martin Reinhardt <contact@martinreinhardt-online.de>
2023-03-03 19:21:16 +0100

    chore: Simple SpringBoot Sample

---
+++ src/main/java/de/m13t/buildah/BuildahApplication.java
@@ -0,0 +1,13 @@
+package de.m13t.buildah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/m13t/buildah/BuildahApplicationTests.java
@@ -0,0 +1,13 @@
+package de.m13t.buildah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
